5e6a53e79adbe0173d92cc18567d4b3ca631ec18
ace2alchemist88 <140344447+ace2alchemist88@users.noreply.github.com>
2023-07-23 21:59:42 +0300

    Update streamlit_app.py
    added menu an dheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-stremlit.text('Oats and Muesli')
-streamlit.text('Boiled Eggs and Cornflakes')
-streamlit.text('Fruits and Juices')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
