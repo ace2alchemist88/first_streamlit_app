99ff13893b8c2017605d5ced1706c6a743738ba5
ace2alchemist88 <140344447+ace2alchemist88@users.noreply.github.com>
2023-07-23 21:57:14 +0300

    Update streamlit_app.py
    added text and menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents new Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+stremlit.text('Oats and Muesli')
+streamlit.text('Boiled Eggs and Cornflakes')
+streamlit.text('Fruits and Juices')
